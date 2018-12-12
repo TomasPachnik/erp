e23c870a258ac1a43721f834d7c48a1dc108db8c
Tomáš Páchnik <tomas.pachnik@gmail.com>
2018-12-12 17:27:09 +0100

    jasper wip

--- src/main/java/sk/tomas/erp/service/InvoiceService.java
+++ src/main/java/sk/tomas/erp/service/InvoiceService.java
@@ -1,4 +1,16 @@
 package sk.tomas.erp.service;
 
+import net.sf.jasperreports.engine.JRException;
+import sk.tomas.erp.bo.Invoice;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 public interface InvoiceService {
+
+    Invoice getById(UUID uuid);
+
+    File generatePdf(Invoice invoice,String name) throws JRException, IOException;
+
 }
--- src/main/java/sk/tomas/erp/service/impl/InvoiceServiceImpl.java
+++ src/main/java/sk/tomas/erp/service/impl/InvoiceServiceImpl.java
@@ -1,7 +1,32 @@
 package sk.tomas.erp.service.impl;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRLoader;
+import sk.tomas.erp.bo.Invoice;
 import sk.tomas.erp.service.InvoiceService;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class InvoiceServiceImpl implements InvoiceService {
+
+    @Override
+    public Invoice getById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public File generatePdf(Invoice invoice, String name) throws JRException, IOException {
+        InputStream inputStream = null;
+        JasperReport report = (JasperReport) JRLoader.loadObject(inputStream);
+        JasperPrint print = JasperFillManager.fillReport(report, new HashMap<>());
+        File pdf = File.createTempFile(name + ".", ".pdf");
+        JasperExportManager.exportReportToPdfStream(print, new FileOutputStream(pdf));
+        return pdf;
+    }
 
 }
