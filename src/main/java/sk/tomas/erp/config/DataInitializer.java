42b5364ba4c6fdaa2292aa131c164faed984b23c
Tomáš Páchnik <tomas.pachnik@gmail.com>
2018-12-21 19:40:01 +0100

    default pass from property

--- src/main/java/sk/tomas/erp/config/DataInitializer.java
+++ src/main/java/sk/tomas/erp/config/DataInitializer.java
@@ -2,6 +2,7 @@ package sk.tomas.erp.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,9 @@ public class DataInitializer implements CommandLineRunner {
 
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
+
+    @Value("${admin.default.password:password}")
+    private String password;
 
     @Autowired
     public DataInitializer(PasswordEncoder passwordEncoder, UserRepository users) {
@@ -34,7 +38,7 @@ public class DataInitializer implements CommandLineRunner {
                     .login("admin")
                     .name("admin")
                     .email("admin@admin")
-                    .password(this.passwordEncoder.encode("password"))
+                    .password(this.passwordEncoder.encode(password))
                     .enabled(true)
                     .roles(Arrays.asList("ROLE_USER", "ROLE_ADMIN"))
                     .build()
